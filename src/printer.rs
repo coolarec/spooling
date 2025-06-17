fe5560ade898740d014d05597a78c65f361a0ed1
coolarec <coolarec@qq.com>
2025-06-17 13:14:36 +0800

    增加打印机的打印功能

--- src/printer.rs
+++ src/printer.rs
@@ -1,3 +1,4 @@
+use genpdf::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum PrinterStatus {
@@ -10,17 +11,35 @@ pub struct Printer {
     status:PrinterStatus,
 }
 
-impl Job{
+impl Printer{
     
     //初始时打印机为空，可以打印东西
     pub fn new()->Self{
         Self{
-            status:Free,
+            status:PrinterStatus::Free,
         }
     }
 
-    //模拟演示
-    fn print_file(file:String)->bool{
+    //模拟打印功能
+    pub fn print_file(&self,code: &str,file_name:&str) {
+        //加载字体
+        let font_family =
+            fonts::from_files("./fonts", "MapleMono", None).expect("Failed to load font family");
 
+        let mut doc = Document::new(font_family);
+        doc.set_title("Demo document");
+        let mut decorator = genpdf::SimplePageDecorator::new();
+        decorator.set_margins(10);
+        doc.set_page_decorator(decorator);
+
+        for line in code.lines() {
+            let p = genpdf::elements::Paragraph::new(line);
+            doc.push(p);
+        }
+
+        doc.render_to_file(&format!("{}.pdf", file_name))
+            .expect("Failed to write PDF file");
     }
+
+
 }
